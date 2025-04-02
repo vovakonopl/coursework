ac139b6d9262af84d3677a375d6ffec854726674
Volodymyr Konopl <konopl538@gmail.com>
2025-04-02 21:40:45 +0100

    Add UI to fill initial values on the board; Fix arrow reader.
    - Created a UI board where user can navigate with arrow keys and fill in
      the initial values.
      NOTE: There is a bug with keys that enter a code sequence
      containing digits. E.g. Delete - "ESC[3~". Everything will be ignored,
      but the '3' will be counted as digit entered by user.
    
    - Fixed the mistyped code in read_arrow method. 'C' character for right
      arrow was typed in lower case.

--- include/terminal/terminal_io.h
+++ include/terminal/terminal_io.h
@@ -11,20 +11,30 @@ class Terminal {
     int cell_height;
 
     // Cannonical mode allows to read the input after the Enter button is pressed
-    // We need to disable it to listen every arrow key press
     void disable_canon_mode();
     void enable_canon_mode();
+    // Toggle showing typed characters
+    void disable_echo();
+    void enable_echo();
    
     void clear_input();
     void clear_terminal();
+
+    // Next 2 methods read the input searching for specific key
+    // If they find unexpected char, they put all tacked chars back in the input stream
     ArrowKey read_arrow_key();
-    void cursor_to_cell(int row, int col);
-    void paint_cell(std::string_view bg_color, int row, int col);
+    bool is_backspace();
+    
+    void cursor_to_cell_start(int row, int col);
+    void draw_cell(std::string_view bg_color, int row, int col);
+    void focus_cell(int row, int col);
 
 public:
     Terminal();
 
+    // sets the pointer for actual board
     void set_board(Board *p_board);
+ 
     Board *read_board();
     void render_board();
     void fill_fixed_cells();
--- src/terminal/terminal_io.cpp
+++ src/terminal/terminal_io.cpp
@@ -1,4 +1,5 @@
 #include "terminal/terminal_io.h"
+#include "terminal/arrow_keys.h"
 #include "terminal/codes/borders.h"
 #include "terminal/codes/cursor.h"
 #include "terminal/codes/erase.h"
@@ -8,9 +9,13 @@
 #include <string>
 #include <termios.h>
 #include <unistd.h>
+#include <cctype>
+
+#define MAX_CELL_VAL 99
 
 using std::cout;
 using std::cin;
+using std::endl;
 using std::string;
 using std::string_view;
 
@@ -34,6 +39,22 @@ void Terminal::enable_canon_mode() {
     termios settings;
     tcgetattr(STDIN_FILENO, &settings);
     settings.c_lflag |= ICANON;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &settings);
+}
+
+void Terminal::disable_echo() {
+    termios settings;
+    tcgetattr(STDIN_FILENO, &settings);
+    settings.c_lflag &= ~ECHO;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &settings);
+
+}
+void Terminal::enable_echo() {
+    termios settings;
+    tcgetattr(STDIN_FILENO, &settings);
+    settings.c_lflag |= ECHO;
 
     tcsetattr(STDIN_FILENO, TCSANOW, &settings);
 }
@@ -68,7 +89,7 @@ ArrowKey Terminal::read_arrow_key() {
         case 'B': 
             return ArrowKey::Down;
         
-        case 'c': 
+        case 'C': 
             return ArrowKey::Right;
 
         case 'D': 
@@ -82,10 +103,7 @@ ArrowKey Terminal::read_arrow_key() {
     }
 }
 
-void Terminal::cursor_to_cell(
-    int row,
-    int col
-) {
+void Terminal::cursor_to_cell_start( int row, int col) {
     if (row < 0) {
         row = 0;
     }
@@ -101,13 +119,9 @@ void Terminal::cursor_to_cell(
     cout << cursor_to(line, column);
 }
 
-void Terminal::paint_cell(
-    string_view bg_color,
-    int row,
-    int col
-) {
+void Terminal::draw_cell(string_view bg_color, int row, int col) {
     cout << CURSOR_SAVE;
-    cursor_to_cell(row, col);
+    cursor_to_cell_start(row, col);
     Cell cell = p_board->cell_at(row, col);
     string space(cell_width, ' ');
     string cell_value = space;
@@ -119,18 +133,32 @@ void Terminal::paint_cell(
         cell_value.replace(cell_center, value.length(), value);
     }
 
+    cout << bg_color;
     for (int row = 0; row < cell_height; row++) {
-        cout << bg_color << BOLD;
         if (row == (cell_height - 1) / 2) {
+            if (cell.get_is_fixed()) {
+                cout << BOLD;
+            }
+
             cout << cell_value;
         } else {
             cout << space;
         }
 
-        cout << RESET_ALL << cursor_left(cell_width) << cursor_down(1);
+        cout << cursor_left(cell_width) << cursor_down(1);
     }
 
-    cout << CURSOR_RESTORE; 
+    cout << RESET_ALL CURSOR_RESTORE; 
+}
+
+void Terminal::focus_cell(int row, int col) {
+    cursor_to_cell_start(row, col);
+    draw_cell(BG_LIGHT_GRAY, row, col);
+
+    int center_x = (cell_width - 1) / 2;
+    int center_y = (cell_height - 1) / 2;
+    cursor_right(center_x);
+    cursor_down(center_y);
 }
 
 // =-=-=-=-=-=-=-= Public methods =-=-=-=-=-=-=-= 
@@ -195,7 +223,7 @@ void print_border_line(
             cout << split_char; 
         }
     }
-    cout << last_char << std::endl;
+    cout << last_char << endl;
 }
 
 void Terminal::render_board() {
@@ -226,15 +254,7 @@ void Terminal::render_board() {
 
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++) {
-            int center_x = (cell_width - 1) / 2;
-            int center_y = (cell_height - 1) / 2;
-            cursor_to_cell(row, col);
-            cout << cursor_right(center_x) << cursor_down(center_y);
-            
-            int num = p_board->cell_at(row, col).get_value();
-            if (num != 0) {
-                cout << num;
-            }
+            draw_cell("", row, col);
         }
     }
 
@@ -242,19 +262,136 @@ void Terminal::render_board() {
 }
 
 void Terminal::fill_fixed_cells() {
-    string finish_msg = FONT_ORANGE "[ Finish ]" RESET_ALL;
-    cout << CURSOR_SAVE FONT_ORANGE << finish_msg << std::endl;
-    cout << "Use the arrow keys to navigate between the cells and enter some fixed numbers (0 - 99).\n";
+    render_board();
+    string finish_btn ="[ Finish ]";
+    cout << FONT_ORANGE << finish_btn << RESET_ALL << endl;
+    cout << "Use the arrow keys to navigate between the cells and enter some fixed numbers "
+        << "(0 - " << MAX_CELL_VAL << ").\n";
     cout << CURSOR_INVISIBLE;
-
-    // current cursor position
-    // cursor_to_cell();
   
-    int current_row = 0;
-    int current_col = 0;
+    const int rows = p_board->get_rows();
+    const int cols = p_board->get_cols();
+    const int board_height = 1 + rows * (cell_height + 1); // +1 for boarder lines
 
+    focus_cell(0, 0);
+    int curr_row = 0;
+    int curr_col = 0;
+ 
+    // listen each button + disable showing typed chars
+    disable_canon_mode();
+    disable_echo();
+
+    bool is_btn_in_focus = false;
     while (true) {
-        
-    }
-}
+        int new_row = curr_row;
+        int new_col = curr_col;
+        bool is_moved = true;
 
+        ArrowKey arrow = read_arrow_key();
+        switch (arrow) {
+            case ArrowKey::Up:
+                new_row--;
+                break;
+
+            case ArrowKey::Down:
+                new_row++;
+                break;
+
+            case ArrowKey::Left:
+                new_col--;
+                break;
+
+            case ArrowKey::Right:
+                new_col++;
+                break;
+
+            case ArrowKey::None:
+                is_moved = false;
+                break;
+        }
+
+        if (is_btn_in_focus) {
+            // go back to board
+            if (arrow == ArrowKey::Up) {
+                // blur button
+                cout << cursor_to(board_height + 1, 0);
+                cout << FONT_ORANGE << finish_btn << RESET_ALL << endl;
+                
+                focus_cell(curr_row, curr_col);
+                is_btn_in_focus = false;
+                
+                continue;
+            }
+
+            if (arrow != ArrowKey::None) continue;
+
+            char ch = cin.get();
+            if (ch != '\n') continue;
+            
+            // finish filling board
+            break;
+        }
+
+        if (is_moved) {
+            if (
+                new_col < 0 || new_col >= cols ||
+                new_row < 0 || new_row > rows // rows included for the button under the board
+            ) continue;
+
+            // blur the current cell
+            draw_cell("", curr_row, curr_col);
+            
+            // button focused
+            if (new_row == rows) {
+                is_btn_in_focus = true;
+                cout << cursor_to(board_height + 1, 0);
+                cout << BG_ORANGE BOLD << finish_btn << RESET_ALL << endl;
+                
+                continue;
+            }
+
+            curr_col = new_col;
+            curr_row = new_row;
+            focus_cell(curr_row, curr_col);
+            
+            continue;
+        }
+
+        // handle key press (add value or ignore if it isn't digit)
+        Cell &cell = p_board->cell_at(curr_row, curr_col);
+        char ch = cin.get();
+
+        // ==============
+
+        // if backspace or delete char
+        if (ch == '\b' || ch == 127) {
+            int value = cell.get_value() / 10;
+            bool is_fixed = value; // 0 == false
+            cell = Cell(value, is_fixed);
+
+            focus_cell(curr_row, curr_col);
+            continue;
+        }
+
+        if (!std::isdigit(ch)) continue;
+
+        string value_str = std::to_string(cell.get_value());
+        // set max value if there are already 2 digits
+        if (value_str.length() >= 2) {
+            cell = Cell(MAX_CELL_VAL, true);   
+        } else {
+            // ch - '0' returns this digit in int representation
+            int digit = (ch - '0');
+            int value = cell.get_value() * 10 + digit;
+            bool is_fixed = value;
+            cell = Cell(value, is_fixed);
+        }
+
+        focus_cell(curr_row, curr_col);
+    }
+
+    // restore terminal settings
+    enable_canon_mode();
+    enable_echo();
+    cout << cursor_to(board_height + 1, 0) << RESET_ALL ERASE_CURSOR_TO_SCREEN_END CURSOR_VISIBLE;
+}
