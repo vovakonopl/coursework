58114ca96ba63258807b2e9455ec542c5a373602
Volodymyr Konopl <konopl538@gmail.com>
2025-05-12 14:59:45 +0100

    Add manual solving mode

---
+++ include/solver/coord_queue.h
@@ -0,0 +1,29 @@
+#ifndef COORD_QUEUE_H
+#define COORD_QUEUE_H
+
+#include "board/coordinate.h"
+
+class CQNode {
+    Coord value;
+    CQNode *next;
+
+public: 
+    CQNode(Coord coord);
+    friend class CoordQueue;
+}; 
+
+class CoordQueue {
+    int size;
+    CQNode *head;
+    CQNode *tail;
+
+public:
+    CoordQueue();
+    ~CoordQueue();
+
+    int get_size();
+    void enqueue(Coord coord);
+    Coord dequeue();
+};
+
+#endif
